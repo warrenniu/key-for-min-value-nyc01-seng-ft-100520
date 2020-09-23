b4227e7487c4d9e4f78c118d979139de4dea6842
Warren Niu <warrenniu@gmail.com>
2020-09-23 14:45:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |key, value|
       if name_hash.empty?
         return nil
-      if min_key == ""
+      elsif min_key == ""
         min_key = key
       elsif name_hash[key] < name_hash[min_key]
         min_key = key
