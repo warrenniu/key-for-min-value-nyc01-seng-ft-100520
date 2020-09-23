df35dc93ffce99a0d38b89029c84e3a4e54bfae8
Warren Niu <warrenniu@gmail.com>
2020-09-23 14:04:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-total = 0
+i = 0
 min_value = {}
   name_hash.each do |key, value|
     if value < name_hash[:key]
       min_value << value
-      total += 1
+      i += 1
   end
 end
 min_value
